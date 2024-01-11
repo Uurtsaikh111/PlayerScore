32b1459a74ac1fec25032a61eb65c6e9ba38ba33
Uurtsaikh <uuree.i4uuree.com>
2024-01-12 00:18:47 +0800

    Jan12

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import "./styles/global.css";
+import "./styles/container.css";
+import "./styles/card.css";
+import "./styles/buttons.css";
+
 export default function App() {
   return (
-    <div>
-      <p>hello</p>
+    <div className="container">
+      <div className="header">
+        <h1>Play Score</h1>
+        <img src="" alt="" />
+      </div>
+      <p>Hidden in the middle of text</p>
+      <div className="body">
+        <div className="card"></div>
+      </div>
     </div>
   );
 }
